4678c457e05ee311e1f0c4e5949872942f010707
Seraphine Young <seraphine_ene@yahoo.com>
2019-02-17 20:44:58 +0000

    Added info to read me file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faHeart, faHeartBroken } from "@fortawesome/free-solid-svg-icons";
